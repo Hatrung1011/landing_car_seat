be4dfb1d619b44b76d28779e25a008aa5dfa10b2
trungnh <trungnh@htpgroup.com.vn>
2026-03-09 14:50:56 +0700

    Fix iframe JSX syntax in Footer, add map, update contact info

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -91,7 +91,7 @@ const Footer = () => {
                     <div className="footer__col footer__col--map">
                         <h4 className="footer__col-title">Bản Đồ</h4>
                         <div className="footer__map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.5982294577925!2d105.3088064!3d21.2081136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134871fbfe96a17%3A0x8fd5796b136f21ae!2zQmFieSBDYXIgU2VhdCBOaOG6rXQgSOG6oSBTdG9yZQ!5e0!3m2!1svi!2s!4v1773042211110!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.5982294577925!2d105.3088064!3d21.2081136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3134871fbfe96a17%3A0x8fd5796b136f21ae!2zQmFieSBDYXIgU2VhdCBOaOG6rXQgSOG6oSBTdG9yZQ!5e0!3m2!1svi!2s!4v1773042211110!5m2!1svi!2s" width="100%" height="250" style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
